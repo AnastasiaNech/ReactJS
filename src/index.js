c02d888fc29d1452d46b485d8a253303edbd7be1
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2021-09-08 16:47:07 +0300

    Lesson 2 Part 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const el = React.createElement(
+//     'h1',
+//     null,
+//     'Hello World,ReactJS!'
+// )
+const AppHeader = () => {
+    return (
+        <h1 className="App-header">Hello World,ReactJS!</h1>
+    );
+}
+
+const AppInput = () => {
+    const placeholder = "Type text...."
+    return (
+        <lable htmlfor="search">
+            <input
+                id="search"
+                placeholder ={placeholder}
+            />
+        </lable>
+    );
+}
+
+const AppList = () => {
+    const items = ['Items 1','Items 2', 'Items 3'];
+    const firstItem = <li>Items 0</li>;
+    return (
+        <ul>
+        {
+            firstItem
+        }
+        {
+        items.map(item => <li>{items}</li>)
+        }
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    );
+}
+
+const App = () => {
+    return (
+    <>
+        <AppInput/>
+        <AppHeader/>
+        <AppList/>
+    </>
 );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
 
