a30a59ec20aa69edc641d48d5a82f4f51a619a6c
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2021-09-09 18:32:09 +0300

    Урок 2
    Урок 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Footer from "./components/Footer";
+import Layout from "./components/Layout";
+import Header from "./components/Header";
+import urlBg from "./assets/bg1.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <Header
+                title = "This is new Title"
+                descr= "This is new description!"
+            />
+            <Layout
+                title = "This is new Title"
+                descr= "This is new description!"
+                urlBg ={urlBg}
+            />
+            <Layout
+                title = "This is new Title"
+                descr= "This is new description!"
+                colorBg = 'red'
+            />
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,55 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App";
+import './index.css';
 
-// const el = React.createElement(
-//     'h1',
-//     null,
-//     'Hello World,ReactJS!'
-// )
-const AppHeader = () => {
-    return (
-        <h1 className="App-header">Hello World,ReactJS!</h1>
-    );
-}
-
-const AppInput = () => {
-    const placeholder = "Type text...."
-    return (
-        <lable htmlfor="search">
-            <input
-                id="search"
-                placeholder ={placeholder}
-            />
-        </lable>
-    );
-}
-
-const AppList = () => {
-    const items = ['Items 1','Items 2', 'Items 3'];
-    const firstItem = <li>Items 0</li>;
-    return (
-        <ul>
-        {
-            firstItem
-        }
-        {
-        items.map(item => <li>{items}</li>)
-        }
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    );
-}
-
-const App = () => {
-    return (
-    <>
-        <AppInput/>
-        <AppHeader/>
-        <AppList/>
-    </>
-);
-}
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 
