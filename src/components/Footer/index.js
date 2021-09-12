0ad818c8323d2296ea32a43ebc2b9d416e381227
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2021-09-12 14:50:52 +0300

    homework-3

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,14 @@
 import s from './style.module.css';
 
 const Footer = () => {
-return (
-        <div className={s.footer}>
-            <div className={s.wrapper} >
-                <h3 >THANKS FOR VISITING</h3>
-                <p >© 2021 #ReactMarathon.</p>
+    return (
+        <footer>
+            <div className={s.wrapper}>
+                <h3>THANKS FOR VISITING</h3>
+                <p>© 2021 #ReactMarathon.</p>
             </div>
-        </div>
-)
+        </footer>
+    )
 }
 
 export default Footer;
